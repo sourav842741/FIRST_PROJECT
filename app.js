2c8b610431271daed9276d263aa9c94c142db5a9
sourav kumar <souravkumar85055@gmail.com>
2025-02-05 20:02:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const MONGO_URL = ("mongodb://127.0.0.1:27017/test");
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.get("/",(req,res)=>{
+    res.send("hi,i am root")
+});
+
+app.listen(8080,()=>{
+    console.log("server is listing to port 8080");
+});
